8c8b51d5756b520e6a57fa225d0187e12859e552
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-10-07 08:46:24 +0200

    fix: Update docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://devpath.github.io',
+  url: 'https://devpathgame.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/knowledge-base/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'DevPathGame', // Usually your GitHub org/user name.
-  projectName: 'devpath.github.io', // Usually your repo name.
+  projectName: 'devpathgame.github.io', // Usually your repo name.
   deploymentBranch: 'main',
   trailingSlash: false,
 
