f608e19ab0952f4781b6a1a6004ed6ef7db88f6a
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-10-07 08:43:44 +0200

    conf: Add ghpages deployment workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://devpath.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'DevPathGame', // Usually your GitHub org/user name.
+  projectName: 'devpath.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
